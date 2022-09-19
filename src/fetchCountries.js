88774482cbac6f87f86aa01a2bbb68fd62f54a16
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-09-19 08:14:19 +0200

    Fixed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,10 @@
 export function fetchCountries(searchText) {
   return fetch(
     `https://restcountries.com/v3.1/name/${searchText}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => response.json())
-    .catch(error => {});
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
